f75c4772fb3dde9d6376be21d2e633781138fbff
Mathieu Junes <matyjunes@gmail.com>
2020-07-13 21:50:40 +0200

    add button scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'Accueil', to: 'pages#home' , as: :accueil
+  get 'A propos', to: 'pages#about', as: :about
+  get 'Bilan', to: 'pages#bilan', as: :bilan
+  get 'Missions', to: 'pages#mission', as: :mission
+  get 'Entreprise', to: 'pages#entreprise', as: :entreprise
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
