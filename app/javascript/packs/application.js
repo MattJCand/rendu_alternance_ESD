37eb74f73fe8f86cb7312fd1175e7d6db0344554
Mathieu Junes <matyjunes@gmail.com>
2020-07-14 16:16:42 +0200

    add loader

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -34,4 +34,5 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
    initUpdateNavbarOnScroll();
+   $('#js-loader').fadeOut("slow");
 });
