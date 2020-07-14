69e07a4a9f2418f4b879e1a3e1e5dcb9108d2f1f
Mathieu Junes <matyjunes@gmail.com>
2020-07-14 16:31:10 +0200

    remove loader

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -34,5 +34,4 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
    initUpdateNavbarOnScroll();
-   $('#js-loader').fadeOut("slow");
 });
