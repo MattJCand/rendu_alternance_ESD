cc2e7dc7a8ed6d234dea14163e879dfeec7329f3
Mathieu Junes <matyjunes@gmail.com>
2020-07-15 17:50:07 +0200

    first step writing

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,7 +2,7 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight / 12) {
+      if (window.scrollY >= window.innerHeight / 10) {
         navbar.classList.add('navbar-lewagon-white');
       } else {
         navbar.classList.remove('navbar-lewagon-white');
