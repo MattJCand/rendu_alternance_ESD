c8c5570629af5647365bcd446cef87aad6de6b50
Mathieu Junes <matyjunes@gmail.com>
2020-07-19 12:31:50 +0200

    contact mail

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'contact/new'
   root to: 'pages#home'
   get 'Accueil', to: 'pages#home' , as: :accueil
   get 'A propos', to: 'pages#about', as: :about
   get 'Bilan', to: 'pages#bilan', as: :bilan
   get 'Missions', to: 'pages#mission', as: :mission
   get 'Entreprise', to: 'pages#entreprise', as: :entreprise
+  resources :contacts, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
